da845514a54ed547977efc51bfa37cc779478f2c
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-26 23:37:52 +0300

    feat: prepare auto-reply cooldown indexes

--- src/server/db/system-mailbox-indexes.ts
+++ src/server/db/system-mailbox-indexes.ts
@@ -1,4 +1,5 @@
 import { connectDatabase } from "@/server/db/mongoose";
+import { MailAutoReplyReceipt } from "@/server/models/MailAutoReplyReceipt";
 import { SystemMailbox } from "@/server/models/SystemMailbox";
 import { SystemMailMessage } from "@/server/models/SystemMailMessage";
 import { SystemMailSettings } from "@/server/models/SystemMailSettings";
@@ -9,7 +10,12 @@ export async function prepareSystemMailboxDatabase() {
   if (!promise) {
     promise = (async () => {
       await connectDatabase();
-      await Promise.all([SystemMailbox.createIndexes(), SystemMailMessage.createIndexes(), SystemMailSettings.createIndexes()]);
+      await Promise.all([
+        SystemMailbox.createIndexes(),
+        SystemMailMessage.createIndexes(),
+        SystemMailSettings.createIndexes(),
+        MailAutoReplyReceipt.createIndexes()
+      ]);
     })().catch((error) => {
       promise = null;
       throw error;
