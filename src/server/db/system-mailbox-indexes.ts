1dd6526f4d916ff621229e69ab37da66bba63235
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-26 13:38:59 +0300

    feat(mail): prepare production mailbox indexes

---
+++ src/server/db/system-mailbox-indexes.ts
@@ -0,0 +1,18 @@
+import { connectDatabase } from "@/server/db/mongoose";
+import { SystemMailbox } from "@/server/models/SystemMailbox";
+import { SystemMailMessage } from "@/server/models/SystemMailMessage";
+
+let promise: Promise<void> | null = null;
+
+export async function prepareSystemMailboxDatabase() {
+  if (!promise) {
+    promise = (async () => {
+      await connectDatabase();
+      await Promise.all([SystemMailbox.createIndexes(), SystemMailMessage.createIndexes()]);
+    })().catch((error) => {
+      promise = null;
+      throw error;
+    });
+  }
+  return promise;
+}
