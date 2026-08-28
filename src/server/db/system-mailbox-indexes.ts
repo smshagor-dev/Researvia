f2d009e6521a6774e83a28d0cec607cbdc419c6e
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-28 11:47:06 +0300

    feat(mail): prepare vacation responder indexes

--- src/server/db/system-mailbox-indexes.ts
+++ src/server/db/system-mailbox-indexes.ts
@@ -1,5 +1,7 @@
 import { connectDatabase } from "@/server/db/mongoose";
 import { SystemMailbox } from "@/server/models/SystemMailbox";
+import { SystemMailAutoReply } from "@/server/models/SystemMailAutoReply";
+import { SystemMailAutoReplyThrottle } from "@/server/models/SystemMailAutoReplyThrottle";
 import { SystemMailMessage } from "@/server/models/SystemMailMessage";
 import { SystemMailSettings } from "@/server/models/SystemMailSettings";
 
@@ -9,7 +11,13 @@ export async function prepareSystemMailboxDatabase() {
   if (!promise) {
     promise = (async () => {
       await connectDatabase();
-      await Promise.all([SystemMailbox.createIndexes(), SystemMailMessage.createIndexes(), SystemMailSettings.createIndexes()]);
+      await Promise.all([
+        SystemMailbox.createIndexes(),
+        SystemMailMessage.createIndexes(),
+        SystemMailSettings.createIndexes(),
+        SystemMailAutoReply.createIndexes(),
+        SystemMailAutoReplyThrottle.createIndexes()
+      ]);
     })().catch((error) => {
       promise = null;
       throw error;
